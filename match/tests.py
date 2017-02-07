6dc7c16a208049c48c81e13fe49c8012ef84a215
Matthew Bell <m.bell@cs.ucl.ac.uk>
2017-02-07 19:09:06 +0000

    Add automatic travis deploy

--- match/tests.py
+++ match/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
-
+from match.models import Tag
 # Create your tests here.
+
+class TagTestCase(TestCase):
+    def setUp(self):
+        Tag.objects.create(name="My Tag")
+
+    def test_tag_creation(self):
+        myTag = Tag.objects.get(name="My Tag")
+        self.assertEqual(myTag.name, "My Tag")
