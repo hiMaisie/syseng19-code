d62235965252accffd4c78f034e2e33c4f47c099
Matthew Bell <m.bell@cs.ucl.ac.uk>
2017-03-01 01:32:55 +0000

    Increase testing coverage of OAuth endpoints

--- match/tests/test_api_oauth.py
+++ match/tests/test_api_oauth.py
@@ -8,6 +8,7 @@ from oauth2_provider.models import get_application_model, AccessToken, RefreshTo
 from oauth2_provider.settings import oauth2_settings
 from oauth2_provider.tests.test_utils import TestCaseUtils
 from rest_framework import status
+import json
 
 # Sourced from https://github.com/evonove/django-oauth-toolkit/blob/master/oauth2_provider/tests/test_token_revocation.py
 
@@ -43,20 +44,21 @@ class OAuthTest(TestCaseUtils, TestCase):
         )
         self.assertTrue(token)
 
-    #def test_can_access_user_list_if_admin(self):
-    #    token = AccessToken.objects.create(
-    #        user=self.admin_user,
-    #        token='123456789',
-    #        application=self.application,
-    #        expires=timezone.now() + timedelta(days=1),
-    #        scope='read write',
-    #    )
-    #    auth = self._get_auth_header(token=token.token)
-    #    url = reverse('user-list')
-    #    response = self.client.get(url, HTTP_AUTHORIZATION=auth)
-    #    print(response.content.decode('utf-8'))
-    #    self.assertEqual(response.status_code, status.HTTP_200_OK)
-    #    self.assertTrue(len(json.loads(response.content.decode('utf-8'))) == 3)
+    def test_can_access_user_list_if_admin(self):
+        token = AccessToken.objects.create(
+            user=self.admin_user,
+            token='123456789',
+            application=self.application,
+            expires=timezone.now() + timedelta(days=1),
+            scope='read write',
+        )
+        auth = self._get_auth_header(token=token.token)
+        url = reverse('user-list')
+        response = self.client.get(url, HTTP_AUTHORIZATION=auth)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(len(json.loads(response.content.decode('utf-8'))) == 3)
+
+        //TODO: Add 'me' endpoint
 
     def _get_auth_header(self, token=None):
-        return "Bearer: {0}".format(token or self.access_token.token)
+        return "Bearer {0}".format(token or self.access_token.token)
